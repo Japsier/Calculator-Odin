1dfaad0d004be62fccc6ad8c6eeff3d20c6b36e4
JapSir <japsircode@gmail.com>
2022-10-16 23:06:02 +0200

    add clear and back functionality

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const calculator = document.querySelector(".calculator")
 
 let displayValue = document.querySelector(".input")
 let numberInput = []
+let displayString = null
 
 
 function updateText () {
@@ -15,9 +16,9 @@ function operate(operator, num1, num2) {
         return add(num1, num2);
     } else if (operator == "-") {
         return subtract(num1, num2);
-    } else if (operator == "*") {
+    } else if (operator == "x") {
         return multiply (num1, num2)
-    } else if (operator == "/") {
+    } else if (operator == "%") {
         return divide(num1, num2)
     } else {
         console.log("problem with operator")
@@ -42,14 +43,47 @@ function divide (num1, num2) {
 
 
 
-
+//activates if user clicks anywhere on the calculator
 calculator.addEventListener("click", (e) => {
+    //userinput = class of clicked element
     let userInput = (e.target.classList)
+
+    //checks to see if a button is pressed
     let isButton = (e.target.nodeName === "BUTTON")
     if (!isButton === true) {
         return
     }
-    console.log(userInput)
-    displayValue.innerText += userInput
-    numberInput.push(userInput) 
+    
+
+    //does the back and clear function
+    if (userInput == "back") {
+        //delete one number
+        numberInput.pop()
+        displayString = numberInput.join("")
+        displayValue.innerText = displayString
+        console.log(numberInput)
+        return
+        //return
+    } else if (userInput == "clear") {
+        //delete everything from screen
+        displayValue.innerText = ""
+        numberInput.length = 0
+        return
+        //return
+    }
+    
+
+    //Calling operate function if operator is given
+    if (userInput == "=" || userInput == "-" || userInput == "+" || userInput == "x" || userInput == "%") {
+        console.log("works")
+        operate(userInput, "")
+        return
+    }
+    numberInput.push(userInput)
+    displayString = numberInput.join("")
+
+    displayValue.innerText = displayString
+    //
+    
+    console.table(numberInput)
 })
