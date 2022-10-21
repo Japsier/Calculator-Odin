58285839f2f55c9091dd9a3f6466d447cbd6f233
JapSir <japsircode@gmail.com>
2022-10-21 17:23:44 +0200

    make the calculator work

--- script.js
+++ script.js
@@ -5,43 +5,55 @@ const calculator = document.querySelector(".calculator")
 let displayValue = document.querySelector(".input")
 let numberInput = []
 let displayString = null
+let numbers = []
+let operators = []
+let inputGiven = false
+let operatorGiven = false
+
 
 
 function updateText () {
     displayValue.innerText = numberInput;
 }
 
-function operate(operator) {
-    if (operator == "+") {
-        return add();
-    } else if (operator == "-") {
-        return subtract();
-    } else if (operator == "x") {
-        return multiply ()
-    } else if (operator == "%") {
-        return divide()
-    } else {
-        console.log("problem with operator")
+function operate() {
+    let result = numbers[0]
+    for (let i = 0; i < operators.length; i++) {
+        let num = i + 1
+        
+        if (operators[i] == "+") {
+            result = add(result, num);
+        } else if (operators[i] == "-") {
+            result = subtract(result, num);
+        } else if (operators[i] == "x") {
+            result = multiply (result, num)
+        } else if (operators[i] == "%") {
+            result = divide(result, num)
+        } else {
+            console.log("problem with operator")
+        }
     }
+    return result
+
 }
-function add () {
-    let result = "temp"
-    console.log("add")
+function add (result, num) {
+    result += numbers[num]
+    console.log(result)
     return result
 }
-function subtract () {
-    let result =  "temp"
-    console.log("sub")
+function subtract(result, num) {
+    result -= numbers[num]
+    console.log(result)
     return result
 }
-function multiply () {
-    let result =  "temp"
-    console.log("mult")
+function multiply (result, num) {
+    result *= numbers[num]
+    console.log(result, num)
     return result
 }
-function divide () {
-    let result =  "temp"
-    console.log("div")
+function divide (result, num) {
+    result /=  numbers[num]
+    console.log(result)
     return result
 }
 
@@ -51,6 +63,7 @@ function divide () {
 calculator.addEventListener("click", (e) => {
     //userinput = class of clicked element
     let userInput = (e.target.classList)
+
 
     //checks to see if a button is pressed
     let isButton = (e.target.nodeName === "BUTTON")
@@ -72,22 +85,56 @@ calculator.addEventListener("click", (e) => {
         //delete everything from screen
         displayValue.innerText = ""
         numberInput.length = 0
+        numbers.length = 0
+        operators.length = 0
         return
         //return
     }
     
 
     //Calling operate function if operator is given
-    if (userInput == "=" || userInput == "-" || userInput == "+" || userInput == "x" || userInput == "%") {
-        console.log("works")
-        operate(userInput, "")
+    if (userInput == "-" || userInput == "+" || userInput == "x" || userInput == "%") {
+        if (inputGiven == true) {
+            numbers.push(parseFloat(displayString))//bug
+        }
+        if (operatorGiven == false) {
+            operators.push(userInput)
+        }
+
+        console.table(numbers)
+        console.table(operators)
+        numberInput.length = 0
+        displayValue.innerText = ""
+        operatorGiven = true
+        inputGiven = false
+        return
+    }
+    if (userInput == "=") {
+        // = function
+
+        //calls operate function 
+        numbers.push(parseFloat(displayString))
+        console.table(numbers)
+        console.table(operators)
+        let result = operate()
+        numberInput.length = 0
+        numberInput[0] = result
+        numbers.length = 0
+        numbers[0] = result
+        operators.length = 0
+        inputGiven = false
+        operatorGiven = false
+        displayValue.innerText = result
         return
     }
     numberInput.push(userInput)
     displayString = numberInput.join("")
 
     displayValue.innerText = displayString
+    inputGiven = true
+    operatorGiven = false
+    console.log(inputGiven)
     //
     
-    console.table(numberInput)
+    //console.table(numberInput)
 })
